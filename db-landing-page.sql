09bcfd0de6ea57f8760302b720e753d2a0d1b50f
Ridho Suhaebi Arrowi <ridhorezi1212@gmail.com>
2022-11-20 10:42:57 +0700

    add database landing page

--- db-landing-page.sql
+++ db-landing-page.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 16, 2022 at 08:09 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Waktu pembuatan: 20 Nov 2022 pada 04.41
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Struktur dari tabel `abouts`
+--
+
+CREATE TABLE `abouts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `quote` varchar(255) NOT NULL,
+  `url_video` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `abouts`
+--
+
+INSERT INTO `abouts` (`id`, `title`, `quote`, `url_video`, `created_at`) VALUES
+(1, 'test about', 'this is quotes', 'https://www.youtube.com/watch?v=d3ZKSmCWwJw', '2022-11-19 11:52:09');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
 --
 
 CREATE TABLE `admin` (
@@ -34,20 +55,107 @@ CREATE TABLE `admin` (
   `nama_lengkap` varchar(255) NOT NULL,
   `email` varchar(25) NOT NULL,
   `token` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `last_login` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `admin`
+-- Dumping data untuk tabel `admin`
 --
 
-INSERT INTO `admin` (`id`, `username`, `password`, `nama_lengkap`, `email`, `token`, `last_login`) VALUES
-(1, 'admin', '$2y$10$6uIUQ/8VfepIn6qp9CSSueY3GfruEE7qS7dBza.lVfbgnl8mfBPU6', 'Ridho Suhaebi', 'ridhosuhaebi01@gmail.com', NULL, '2022-09-16 12:56:43');
+INSERT INTO `admin` (`id`, `username`, `password`, `nama_lengkap`, `email`, `token`, `created_at`, `last_login`) VALUES
+(1, 'Admin', '$2y$10$JlNfHAVmHUyyzkCQGLtUC.P6zCPqyYk7jIs.s7yMdKMoXp6dZZ7K6', 'Ridho Suhaebi', 'ridhorezi1212@gmail.com', NULL, '2022-11-19 11:41:54', '2022-11-19 11:31:46');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `migrations`
+-- Struktur dari tabel `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`) VALUES
+(1, 'Web Development'),
+(2, 'Mobile Development');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `name`, `email`, `subject`, `message`, `created_at`) VALUES
+(1, 'Joni', 'jonijon12@gmail.com', 'cooperation', 'I am interested in creating a website for my business', '2022-11-19 11:59:46'),
+(2, 'Jeni', 'jenijen12@gmail.com', 'cooperation', 'I am interested in creating a mobile apps for my business', '2022-11-19 11:59:46');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `feedbacks`
+--
+
+CREATE TABLE `feedbacks` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `feedbacks`
+--
+
+INSERT INTO `feedbacks` (`id`, `name`, `email`, `message`, `created_at`) VALUES
+(1, 'julkar', 'juljul@gmail.com', 'please improve the performance of this website again', '2022-11-19 12:02:15'),
+(2, 'baso konci', 'baksokonci12@gmail.com', 'please improve the appearance of this website again', '2022-11-19 12:02:15'),
+(3, 'jaka', 'jakakonci@gmail.com', 'please add a chat feature to make it easier to communicate', '2022-11-19 12:03:33');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `homes`
+--
+
+CREATE TABLE `homes` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `quote` varchar(255) NOT NULL,
+  `video` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `homes`
+--
+
+INSERT INTO `homes` (`id`, `title`, `quote`, `video`, `created_at`) VALUES
+(1, 'test home', 'this is quote', '1668858481_9df9f953f78804f2cb1b.mp4', '2022-11-19 11:48:01');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
 --
 
 CREATE TABLE `migrations` (
@@ -61,19 +169,110 @@ CREATE TABLE `migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `migrations`
+-- Dumping data untuk tabel `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
 (1, '2022-09-16-044652', 'App\\Database\\Migrations\\Admin', 'default', 'App', 1663314707, 1),
 (2, '2022-09-23-160753', 'App\\Database\\Migrations\\ModifyColumnTokenAdmin', 'default', 'App', 1663949522, 2);
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `professions`
+--
+
+CREATE TABLE `professions` (
+  `profession_id` int(11) NOT NULL,
+  `profession_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `professions`
+--
+
+INSERT INTO `professions` (`profession_id`, `profession_name`) VALUES
+(1, 'IT'),
+(2, 'Bussines Analyst');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `services_category_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `quotes` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `services`
+--
+
+INSERT INTO `services` (`id`, `services_category_id`, `title`, `quotes`, `description`, `created_at`) VALUES
+(1, 1, 'test services', 'this is quotes', '                                                                                                                                                                        this is description                                                                    ', '2022-11-19 11:52:55');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `testimonials`
+--
+
+CREATE TABLE `testimonials` (
+  `id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `quote` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `user_profession_id` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `testimonials`
+--
+
+INSERT INTO `testimonials` (`id`, `image`, `quote`, `name`, `user_profession_id`, `created_at`) VALUES
+(1, '1668859047_2c4578251028590d6fea.png', 'this is quotes', 'Mark Zuckerberg', '1', '2022-11-19 11:57:27');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `works`
+--
+
+CREATE TABLE `works` (
+  `id` int(11) NOT NULL,
+  `works_category_id` int(11) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `quote` varchar(250) NOT NULL,
+  `image` varchar(250) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `works`
+--
+
+INSERT INTO `works` (`id`, `works_category_id`, `title`, `quote`, `image`, `created_at`) VALUES
+(1, 1, 'test works', 'this is quotes', '1668858841_745167bfa064f20fef5a.png', '2022-11-19 11:54:01');
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin`
+-- Indeks untuk tabel `abouts`
+--
+ALTER TABLE `abouts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`),
@@ -81,26 +280,128 @@ ALTER TABLE `admin`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indexes for table `migrations`
+-- Indeks untuk tabel `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indeks untuk tabel `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `homes`
+--
+ALTER TABLE `homes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indeks untuk tabel `professions`
+--
+ALTER TABLE `professions`
+  ADD PRIMARY KEY (`profession_id`);
+
+--
+-- Indeks untuk tabel `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `testimonials`
+--
+ALTER TABLE `testimonials`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `works`
+--
+ALTER TABLE `works`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT untuk tabel `abouts`
+--
+ALTER TABLE `abouts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `migrations`
+-- AUTO_INCREMENT untuk tabel `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `homes`
+--
+ALTER TABLE `homes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `professions`
+--
+ALTER TABLE `professions`
+  MODIFY `profession_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `testimonials`
+--
+ALTER TABLE `testimonials`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `works`
+--
+ALTER TABLE `works`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
