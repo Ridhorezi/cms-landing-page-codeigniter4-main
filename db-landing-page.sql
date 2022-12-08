b9909933f2aa5a5c7d956268aa83eb27555f55e8
Ridho Suhaebi Arrowi <ridhorezi1212@gmail.com>
2022-12-08 16:04:17 +0700

    Update DB

--- db-landing-page.sql
+++ db-landing-page.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 20 Nov 2022 pada 04.41
--- Versi server: 10.4.25-MariaDB
--- Versi PHP: 8.1.10
+-- Waktu pembuatan: 08 Des 2022 pada 07.27
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,14 +33,14 @@ CREATE TABLE `abouts` (
   `quote` varchar(255) NOT NULL,
   `url_video` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `abouts`
 --
 
 INSERT INTO `abouts` (`id`, `title`, `quote`, `url_video`, `created_at`) VALUES
-(1, 'test about', 'this is quotes', 'https://www.youtube.com/watch?v=d3ZKSmCWwJw', '2022-11-19 11:52:09');
+(1, 'About <span>Company</span>', 'Learn more about us, its only 30 mins', 'https://www.youtube.com/watch?v=d3ZKSmCWwJw', '2022-12-06 03:15:41');
 
 -- --------------------------------------------------------
 
@@ -52,19 +52,19 @@ CREATE TABLE `admin` (
   `id` int(5) UNSIGNED NOT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `nama_lengkap` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
   `email` varchar(25) NOT NULL,
   `token` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `last_login` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data untuk tabel `admin`
 --
 
-INSERT INTO `admin` (`id`, `username`, `password`, `nama_lengkap`, `email`, `token`, `created_at`, `last_login`) VALUES
-(1, 'Admin', '$2y$10$JlNfHAVmHUyyzkCQGLtUC.P6zCPqyYk7jIs.s7yMdKMoXp6dZZ7K6', 'Ridho Suhaebi', 'ridhorezi1212@gmail.com', NULL, '2022-11-19 11:41:54', '2022-11-19 11:31:46');
+INSERT INTO `admin` (`id`, `username`, `password`, `fullname`, `email`, `token`, `created_at`, `last_login`) VALUES
+(1, 'Admin', '$2y$10$IMm3IbUW6xRxC1HJ/uKCqOhD5ewclQ.PnFCnOWC2o3d5pB8haFS4K', 'Ridho Suhaebi', 'ridhorezi1212@gmail.com', 'e101ff85b638aa7dd7bea0aa15cd73c4', '2022-12-06 15:12:29', '2022-11-19 11:31:46');
 
 -- --------------------------------------------------------
 
@@ -75,7 +75,7 @@ INSERT INTO `admin` (`id`, `username`, `password`, `nama_lengkap`, `email`, `tok
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `category`
@@ -83,7 +83,11 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`category_id`, `category_name`) VALUES
 (1, 'Web Development'),
-(2, 'Mobile Development');
+(2, 'Mobile Development'),
+(3, 'Startup Development\r\n'),
+(4, 'Cyber Scurity'),
+(5, 'UI/UX Design'),
+(6, 'Database Architecture');
 
 -- --------------------------------------------------------
 
@@ -98,15 +102,7 @@ CREATE TABLE `contacts` (
   `subject` varchar(255) NOT NULL,
   `message` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `contacts`
---
-
-INSERT INTO `contacts` (`id`, `name`, `email`, `subject`, `message`, `created_at`) VALUES
-(1, 'Joni', 'jonijon12@gmail.com', 'cooperation', 'I am interested in creating a website for my business', '2022-11-19 11:59:46'),
-(2, 'Jeni', 'jenijen12@gmail.com', 'cooperation', 'I am interested in creating a mobile apps for my business', '2022-11-19 11:59:46');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -120,16 +116,7 @@ CREATE TABLE `feedbacks` (
   `email` varchar(255) NOT NULL,
   `message` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `feedbacks`
---
-
-INSERT INTO `feedbacks` (`id`, `name`, `email`, `message`, `created_at`) VALUES
-(1, 'julkar', 'juljul@gmail.com', 'please improve the performance of this website again', '2022-11-19 12:02:15'),
-(2, 'baso konci', 'baksokonci12@gmail.com', 'please improve the appearance of this website again', '2022-11-19 12:02:15'),
-(3, 'jaka', 'jakakonci@gmail.com', 'please add a chat feature to make it easier to communicate', '2022-11-19 12:03:33');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -143,14 +130,37 @@ CREATE TABLE `homes` (
   `quote` varchar(255) NOT NULL,
   `video` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `homes`
 --
 
 INSERT INTO `homes` (`id`, `title`, `quote`, `video`, `created_at`) VALUES
-(1, 'test home', 'this is quote', '1668858481_9df9f953f78804f2cb1b.mp4', '2022-11-19 11:48:01');
+(1, 'Hi, we are <br> GBVRJ SOLUTIONS TECHNOLOGY', '                                                                                                        Building an extraordinary Mobile App? That\'s not our job. That\'s OUR PASSION                                                                           ', '1670296300_125b5dbcf742c98005c3.mp4', '2022-12-06 03:39:20');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `info`
+--
+
+CREATE TABLE `info` (
+  `id` int(11) NOT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `company_contact` varchar(255) NOT NULL,
+  `company_mail` varchar(50) NOT NULL,
+  `company_location` varchar(255) NOT NULL,
+  `copyright` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `info`
+--
+
+INSERT INTO `info` (`id`, `company_name`, `company_contact`, `company_mail`, `company_location`, `copyright`, `created_at`) VALUES
+(1, 'Gbvrj Solutions Technology', '01212121212', 'gbvrj@gmail.com', ' Ruko Taman Kota Bekasi H-6, Jl. Ki Mangun Sarkoro, Kota Bekasi 17112 - Indonesia', '2022 - Gbrvj Solutions Technology LTD', '2022-12-06 03:31:32');
 
 -- --------------------------------------------------------
 
@@ -166,7 +176,7 @@ CREATE TABLE `migrations` (
   `namespace` varchar(255) NOT NULL,
   `time` int(11) NOT NULL,
   `batch` int(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data untuk tabel `migrations`
@@ -185,7 +195,7 @@ INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`
 CREATE TABLE `professions` (
   `profession_id` int(11) NOT NULL,
   `profession_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `professions`
@@ -193,7 +203,8 @@ CREATE TABLE `professions` (
 
 INSERT INTO `professions` (`profession_id`, `profession_name`) VALUES
 (1, 'IT'),
-(2, 'Bussines Analyst');
+(2, 'Bussines Analyst'),
+(3, 'Project Manager');
 
 -- --------------------------------------------------------
 
@@ -207,15 +218,21 @@ CREATE TABLE `services` (
   `title` varchar(255) NOT NULL,
   `quotes` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
+  `label_icon` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `services`
 --
 
-INSERT INTO `services` (`id`, `services_category_id`, `title`, `quotes`, `description`, `created_at`) VALUES
-(1, 1, 'test services', 'this is quotes', '                                                                                                                                                                        this is description                                                                    ', '2022-11-19 11:52:55');
+INSERT INTO `services` (`id`, `services_category_id`, `title`, `quotes`, `description`, `label_icon`, `created_at`) VALUES
+(1, 1, 'Our <span>Services</span>', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy <br> nibh euismod tincidunt ut laoreet dolore magna.', '                                                                                                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.                                          ', 'laptop', '2022-12-06 03:38:06'),
+(2, 2, 'Our <span>Services</span>', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy <br> nibh euismod tincidunt ut laoreet dolore magna.', '                                                                                                                                                                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod', 'tablet', '2022-12-06 03:43:49'),
+(3, 5, 'Our <span>Services</span>', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy <br> nibh euismod tincidunt ut laoreet dolore magna.', '                                                                                                                                                                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod', 'layers', '2022-12-06 03:43:59'),
+(4, 6, 'Our <span>Services</span>', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy <br> nibh euismod tincidunt ut laoreet dolore magna.', '                                                                                                                                                                                                                                Lorem ipsum dolor sit amet, con', 'database', '2022-12-06 03:46:42'),
+(5, 4, 'Our <span>Services</span>', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy <br> nibh euismod tincidunt ut laoreet dolore magna.', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit<br>', 'magnifier', '2022-12-06 04:07:53'),
+(6, 3, 'Our <span>Services</span>', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy <br> nibh euismod tincidunt ut laoreet dolore magna.', '                                                                                                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit<br>                                                                                   ', 'star', '2022-12-06 04:09:40');
 
 -- --------------------------------------------------------
 
@@ -230,14 +247,29 @@ CREATE TABLE `testimonials` (
   `name` varchar(255) NOT NULL,
   `user_profession_id` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `testimonials`
 --
 
 INSERT INTO `testimonials` (`id`, `image`, `quote`, `name`, `user_profession_id`, `created_at`) VALUES
-(1, '1668859047_2c4578251028590d6fea.png', 'this is quotes', 'Mark Zuckerberg', '1', '2022-11-19 11:57:27');
+(1, '1670296829_c6765d267236412b9e8e.jpg', 'Best Company Builder In The World', 'Jefi Albukhari', '1', '2022-12-06 03:20:29'),
+(2, '1670296966_ea55f9079d4a06fdbb1a.jpg', 'I am very happy with the service from gbvrj solution technology                                                                                                        ', 'Anisatul Ma\'rifah', '2', '2022-12-06 03:22:46'),
+(3, '1670297196_24d8f8d2e298dd98bb87.jpg', '<font color=\"#888888\" face=\"Raleway, sans-serif\"><span style=\"font-size: 24px; background-color: rgb(245, 245, 245);\">Working with the Gbvrj team was a pleasure. They understood what I wanted and created a fantastic mobile app for my company.</span></font', 'Della Krisnauly', '3', '2022-12-06 03:26:36');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `visitor_stats`
+--
+
+CREATE TABLE `visitor_stats` (
+  `id` int(11) NOT NULL,
+  `ip_address` varchar(255) NOT NULL,
+  `user_agent` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -251,15 +283,21 @@ CREATE TABLE `works` (
   `title` varchar(250) NOT NULL,
   `quote` varchar(250) NOT NULL,
   `image` varchar(250) NOT NULL,
+  `filter` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `works`
 --
 
-INSERT INTO `works` (`id`, `works_category_id`, `title`, `quote`, `image`, `created_at`) VALUES
-(1, 1, 'test works', 'this is quotes', '1668858841_745167bfa064f20fef5a.png', '2022-11-19 11:54:01');
+INSERT INTO `works` (`id`, `works_category_id`, `title`, `quote`, `image`, `filter`, `created_at`) VALUES
+(1, 1, 'Our <span>Works</span>', '                                                                                                        Since 2022, we have delivered more than 20 successful web app, mobile app, and other projects. <br> These are the most highlighted apps we have de', '1670301039_510c54ac471dd32ebcdd.jpg', 'web', '2022-12-06 04:30:39'),
+(2, 2, 'Our <span>Works</span>', 'Since 2022, we have delivered more than 20 successful web app, mobile app, and other projects. &lt;br&gt; These are the most highlighted apps we have developed.&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;', '1670300519_e88b8acc1d43e615b4f7.jpg', 'mobile', '2022-12-06 04:21:59'),
+(3, 5, 'Our <span>Works</span>', 'Since 2022, we have delivered more than 20 successful web app, mobile app, and other projects. &lt;br&gt; These are the most highlighted apps we have developed.&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;', '1670300926_9a516b9b131d697b7caf.jpg', 'uiux', '2022-12-06 04:28:46'),
+(4, 6, 'Our <span>Works</span>', '                                                    <span style=\"color: rgb(96, 112, 128); font-family: Nunito;\">Since 2022, we have delivered more than 20 successful web app, mobile app, and other projects. <br> These are the most highlighted apps w', '1670301874_38e1831ce4fb6da51f2b.jpeg', 'database', '2022-12-06 04:44:34'),
+(5, 4, 'Our <span>Works</span>', '<span style=\"color: rgb(96, 112, 128); font-family: Nunito;\">Since 2022, we have delivered more than 20 successful web app, mobile app, and other projects. &lt;br&gt; These are the most highlighted apps we have developed.&nbsp; &nbsp; &nbsp; &nbsp;</', '1670301626_ec4c135f4f6e43484577.png', 'cyberscurity', '2022-12-06 04:40:26'),
+(7, 3, 'Our <span>Works</span>', 'Since 2022, we have delivered more than 20 successful web app, mobile app, and other projects. <br> These are the most highlighted apps we have developed.       ', '1670301781_25da6b12758c30d67518.png', 'startup', '2022-12-06 04:43:44');
 
 --
 -- Indexes for dumped tables
@@ -304,6 +342,12 @@ ALTER TABLE `homes`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `info`
+--
+ALTER TABLE `info`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
@@ -325,6 +369,12 @@ ALTER TABLE `services`
 -- Indeks untuk tabel `testimonials`
 --
 ALTER TABLE `testimonials`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `visitor_stats`
+--
+ALTER TABLE `visitor_stats`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -353,24 +403,30 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT untuk tabel `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `feedbacks`
 --
 ALTER TABLE `feedbacks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `homes`
 --
 ALTER TABLE `homes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `info`
+--
+ALTER TABLE `info`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
@@ -383,25 +439,31 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT untuk tabel `professions`
 --
 ALTER TABLE `professions`
-  MODIFY `profession_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `profession_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `testimonials`
 --
 ALTER TABLE `testimonials`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `visitor_stats`
+--
+ALTER TABLE `visitor_stats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `works`
 --
 ALTER TABLE `works`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
